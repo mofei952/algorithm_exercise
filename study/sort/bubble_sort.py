9cbb03120482ed733882b33e6d1ce3c6c7485539
mofei952 <695741200@qq.com>
2019-05-29 08:57:47 +0800

    update bubble sort

--- study/sort/bubble_sort.py
+++ study/sort/bubble_sort.py
@@ -44,15 +44,15 @@ def bubble_sort2(arr):
         pos = 0
         for j in range(i):
             if arr[j] > arr[j + 1]:
-                pos = j
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                pos = j
         i = pos
-        # print(i)
     return arr
 
 
 @running_time
 def cocktail_sort(arr):
+    """鸡尾酒排序"""
     low = 0
     high = len(arr) - 1
     while low < high:
@@ -88,16 +88,37 @@ def cocktail_sort1(arr):
     return arr
 
 
+@running_time
+def cocktail_sort2(arr):
+    """鸡尾酒排序优化2"""
+    low = 0
+    high = len(arr) - 1
+    while low < high:
+        pos = low
+        for i in range(low, high):
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                pos = i
+        high = pos
+        pos = high
+        for i in range(high, low, -1):
+            if arr[i] < arr[i - 1]:
+                arr[i], arr[i - 1] = arr[i - 1], arr[i]
+                pos = i
+        low = pos
+    return arr
+
+
 if __name__ == '__main__':
     count = 1000
-    # 对于随机序列，鸡尾酒排序优化1稍快一点
-    data = [random.randint(-count, count) for i in range(count)]
-    # 对于已经有序的序列，冒泡排序优化1的效率比一般冒泡快
+    # 对于随机序列，鸡尾酒排序优化12稍快一点
+    # data = [random.randint(-count, count) for i in range(count)]
+    # 对于已经有序的序列，冒泡排序优化1的效率比一般冒泡快，鸡尾酒排序优化1比鸡尾酒排序快
     # data = [i for i in range(count)]
     # 对于后面大部分有序的序列，优化2的效率比优化1高
     # data = [1 if i == 30 else i for i in range(count)]
-    # 对于前面大部分有序的序列，鸡尾酒排序优化1比其他几种都快
-    # data = [i for i in range(count)] + [1, 5, 2, 4]
+    # 对于前面大部分有序的序列，鸡尾酒排序优化12比其他几种都快
+    data = [i for i in range(count)] + [1]
 
     sort_data = sorted(data)
     assert bubble_sort(copy(data)) == sort_data
@@ -105,3 +126,4 @@ if __name__ == '__main__':
     assert bubble_sort2(copy(data)) == sort_data
     assert cocktail_sort(copy(data)) == sort_data
     assert cocktail_sort1(copy(data)) == sort_data
+    assert cocktail_sort2(copy(data)) == sort_data
